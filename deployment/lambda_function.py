e1c68d9b0c7911e091e163253c7ad2f7a744f5fa
Emilie Xu <31358826+emilie-xu@users.noreply.github.com>
2023-02-26 21:13:58 -0500

    Upload packages folder
    upload l2 deployment packages folder

---
+++ deployment/lambda_function.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+REGION = 'TODO'
+HOST = 'TODO'
+INDEX = 'TODO'
+
+
+def lambda_handler(event, context):
+    print('Received event: ' + json.dumps(event))
+
+    results = query('Aaron Rodgers')
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+        },
+        'body': json.dumps({'results': results})
+    }
+
+
+def query(term):
+    q = {'size': 5, 'query': {'multi_match': {'query': term}}}
+
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+                        http_auth=get_awsauth(REGION, 'es'),
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection)
+
+    res = client.search(index=INDEX, body=q)
+    print(res)
+
+    hits = res['hits']['hits']
+    results = []
+    for hit in hits:
+        results.append(hit['_source'])
+
+    return results
+
+
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
